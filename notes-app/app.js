04a4d14a5bd66a2937d559e9162f9400908adb68
tackyunicorn <joshjoseph97@gmail.com>
2019-04-10 18:22:27 +0530

    :heavy_check_mark: yargs parsing 1

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,44 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
+// Customize yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-  console.log('Adding note!')
-} else if (command === 'remove') {
-  console.log('Removing note!')
-}
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+	console.log('Adding a new note!')
+  }
+})
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+	console.log('Removing the note!')
+  }
+})
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List the notes',
+  handler: function () {
+	console.log('Listing the notes!')
+  }
+})
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+	console.log('Reading a note!')
+  }
+})
+
+console.log(yargs.argv)
