f294413dd794595e9bbbf1c355514a92d601c93a
tackyunicorn <joshjoseph97@gmail.com>
2019-04-10 15:49:18 +0530

    :heavy_check_mark: importing npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,7 @@
-// Challenge Solved
-
+const validator = require('validator')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 
 console.log(msg)
+console.log(validator.isURL('https/mead.io'))
