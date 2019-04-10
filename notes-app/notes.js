6d2fc340a0ac3210ab1d91c4ff4859defe47f781
tackyunicorn <joshjoseph97@gmail.com>
2019-04-10 21:47:27 +0530

    :hammer: refactoring to use arrow syntax

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,15 +1,13 @@
 const chalk = require('chalk')
 const fs = require('fs')
 
-const getNotes = function () {
+const getNotes = () => {
 	return 'Your notes...'
 }
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
 	const notes = loadNotes()
-	const duplicateNotes = notes.filter(function (note) {
-		return note.title === title
-	})
+	const duplicateNotes = notes.filter((note) => note.title === title)
 
 	if (duplicateNotes.length === 0) {
 		notes.push({
@@ -23,11 +21,9 @@ const addNote = function (title, body) {
 	}
 }
 
-const removeNote = function (title) {
+const removeNote = (title) => {
 	const notes = loadNotes()
-	const keepNotes = notes.filter(function (note) {
-		return note.title !== title
-	})
+	const keepNotes = notes.filter((note) => note.title !== title)
 
 	if (keepNotes.length != notes.length) {
 		saveNotes(keepNotes)
@@ -37,12 +33,12 @@ const removeNote = function (title) {
 	}
 }
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
 	const dataJSON = JSON.stringify(notes)
 	fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function () {
+const loadNotes = () => {
 	try {
 		const dataBuffer = fs.readFileSync('notes.json')
 		const dataJSON = dataBuffer.toString()
