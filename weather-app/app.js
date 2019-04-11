c39576a1689c3682f107597f0709881755cfc772
tackyunicorn <joshjoseph97@gmail.com>
2019-04-11 22:15:16 +0530

    :partly_sunny: custom http requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,10 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/8b02b1368b6bf72267981515c35f8f5a/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/8b02b1368b6bf72267981515c35f8f5a/37.8267,-122.4233?units=si&lang=ja'
 
 request({
-	url: url
+	url: url,
+	json: true
 }, (error, response) => {
-	const data = JSON.parse(response.body)
-	console.log(data.currently)
+	console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
 })
