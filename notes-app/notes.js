c41b663ca301da3b2d9c21ef178540e4008f12d5
tackyunicorn <joshjoseph97@gmail.com>
2019-04-11 14:43:53 +0530

    :heavy_check_mark: reading a note

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,15 +1,11 @@
 const chalk = require('chalk')
 const fs = require('fs')
 
-const getNotes = () => {
-	return 'Your notes...'
-}
-
 const addNote = (title, body) => {
 	const notes = loadNotes()
-	const duplicateNotes = notes.filter((note) => note.title === title)
+	const duplicateNote = notes.find((note) => note.title === title)
 
-	if (duplicateNotes.length === 0) {
+	if (!duplicateNote) {
 		notes.push({
 			title: title,
 			body: body
@@ -40,6 +36,18 @@ const listNotes = () => {
 	notes.forEach((note) => console.log(chalk.green.inverse.bold(note.title)))
 }
 
+const readNote = (title) => {
+	const notes = loadNotes()
+	const note = notes.find((note) => note.title === title)
+
+	if(note) {
+		console.log(chalk.green.inverse.bold(note.title))
+		console.log(note.body)
+	} else {
+		console.log(chalk.red.inverse.bold('Note not found!'))
+	}
+}
+
 const saveNotes = (notes) => {
 	const dataJSON = JSON.stringify(notes)
 	fs.writeFileSync('notes.json', dataJSON)
@@ -56,8 +64,8 @@ const loadNotes = () => {
 }
 
 module.exports = {
-	getNotes: getNotes,
 	addNote: addNote,
 	removeNote: removeNote,
-	listNotes: listNotes
+	listNotes: listNotes,
+	readNote: readNote
 }
