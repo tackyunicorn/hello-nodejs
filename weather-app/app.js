f0b6ebe42b1da2fad4a25d2bd415036d628769dc
tackyunicorn <joshjoseph97@gmail.com>
2019-04-12 15:00:33 +0530

    :partly_sunny: callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,19 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Boston', (error, data) => {
-	console.log('Error: ' + error)
-	console.log('Data: ', data)
-})
-
-forecast(-75.7088, 44.1545, (error, data) => {
-	console.log('Error: ', error)
-	console.log('Data: ', data)
-})
+if (process.argv[2]) {
+	geocode(process.argv[2], (error, data) => {
+		if (error) {
+			return console.log(error)
+		}
+		forecast(data.latitude, data.longitude, (error, forecastData) => {
+			if (error) {
+				return console.log(error)
+			}
+			console.log(data.location)
+			console.log(forecastData)
+		})
+	})
+} else {
+	console.log('Please enter a location')
+}
