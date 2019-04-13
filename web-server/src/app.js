d81571343fb47f4d33359fe9149040529ca1a23f
tackyunicorn <joshjoseph97@gmail.com>
2019-04-13 19:30:01 +0530

    :globe_with_meridians: building a json http endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -45,9 +47,24 @@ app.get('/weather', (req, res) => {
 			error: 'You must provide an address!'
 		})
 	}
-	res.send({
-		forecast: 'It is snowing',
-		address: req.query.address
+	geocode(req.query.address, (error, { latitude, longitude, location }) => {
+		if (error) {
+			return res.send({
+				error
+			})
+		}
+		forecast(latitude, longitude, (error, forecastData) => {
+			if(error) {
+				return res.send({
+					error
+				})
+			}
+			res.send({
+				forecast: forecastData,
+				location,
+				address: req.query.address
+			})
+		})
 	})
 })
 
