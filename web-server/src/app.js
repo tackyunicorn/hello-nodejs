732959e576b488b34c726ea2aae3e9dcb865cdc1
tackyunicorn <joshjoseph97@gmail.com>
2019-04-13 17:37:22 +0530

    :globe_with_meridians: 404 pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -46,6 +46,22 @@ app.get('/weather', (req, res) => {
 	})
 })
 
+app.get('/help/*', (req, res) => {
+	res.render('404', {
+		title: '404',
+		errorMessage: 'Help article not found!',
+		name: 'Joshua'
+	})
+})
+
+app.get('*', (req, res) => {
+	res.render('404', {
+		title: '404',
+		errorMessage: 'Page not found!',
+		name: 'Joshua'
+	})
+})
+
 app.listen(3000, () => {
 	console.log('Server is up on port 3000')
 })
